df92f9e1b879def84935ccb71232841baff33140
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-05-21 17:51:23 +0300

    Create TicTacToe server and client and connect to each other

--- tic_tac_toe/tic_tac_toe_client.py
+++ tic_tac_toe/tic_tac_toe_client.py
@@ -8,3 +8,20 @@ import socket
 from socket_helper.client_socket import *
 from socket_helper.server_socket import *
 
+class TicTacToeClient:
+    table = ["" for i in range(9)]
+
+    def __init__(self, server_host):
+        self.client_socket = ClientSocket.connect_to_server(server_host, 65001)
+
+    def close(self):
+        if self.client_socket != None:
+            self.client_socket.close()
+
+    def __del__(self):
+        self.close()
+
+if __name__ == "__main__":
+    client = TicTacToeClient("127.0.0.1")
+
+    client.close()
--- tic_tac_toe/tic_tac_toe_server.py
+++ tic_tac_toe/tic_tac_toe_server.py
@@ -8,3 +8,30 @@ import socket
 from socket_helper.client_socket import *
 from socket_helper.server_socket import *
 
+class TicTacToeServer:
+    table = ["" for i in range(9)]
+
+    def __init__(self):
+        self.server_socket = ServerSocket(65001)
+
+    def wait_for_tic_tac_toe_client(self):
+        self.client_socket = self.server_socket.wait_for_client()
+
+    def close(self):
+        if self.server_socket != None:
+            self.server_socket.close()
+
+        if self.client_socket != None:
+            self.client_socket.close()
+
+    def __del__(self):
+        self.close()
+
+    
+
+if __name__ == "__main__":
+    server = TicTacToeServer()
+    server.wait_for_tic_tac_toe_client()
+
+    server.close()
+
