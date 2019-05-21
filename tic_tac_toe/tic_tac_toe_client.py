cfe2a0e74d909b2bc10505df14616cedfc640d83
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-05-21 18:24:52 +0300

    Implement main game loop

--- tic_tac_toe/tic_tac_toe_client.py
+++ tic_tac_toe/tic_tac_toe_client.py
@@ -21,16 +21,46 @@ class TicTacToeClient:
     def __del__(self):
         self.close()
 
+    @staticmethod
+    def cell_content(s, i):
+        if s == "":
+            return str(i + 1)
+
+        return s
+
     def show_table(self):
         print()
         print("Current table:")
 
         for i in range(3):
-            print(self.table[3 * i], self.table[3 * i + 1], self.table[3 * i + 2])
+            print(TicTacToeClient.cell_content(self.table[3 * i], 3 * i), \
+                TicTacToeClient.cell_content(self.table[3 * i + 1], 3 * i + 1),\
+                TicTacToeClient.cell_content(self.table[3 * i + 2], 3 * i + 2))
 
         print()
 
+    def enter(self):
+
+        while True:
+            server_cell = self.client_socket.recv_int()
+            self.table[server_cell - 1] = "x"
+            self.show_table()         
+
+            print("Choose a cell (from 1 to 9): ", end=" ")
+            cell = int(input())
+
+            assert 1 <= cell <= 9
+
+            if self.table[cell - 1] != "x":
+                self.table[cell - 1] = "o"
+
+            self.show_table()
+            self.client_socket.send_int(cell)
+
+
 if __name__ == "__main__":
     client = TicTacToeClient("127.0.0.1")
+
+    client.enter()
 
     client.close()
--- tic_tac_toe/tic_tac_toe_server.py
+++ tic_tac_toe/tic_tac_toe_server.py
@@ -27,21 +27,57 @@ class TicTacToeServer:
     def wait_for_tic_tac_toe_client(self):
         self.client_socket = self.server_socket.wait_for_client()
 
+    @staticmethod
+    def cell_content(s, i):
+        if s == "":
+            return str(i + 1)
+
+        return s
+
     def show_table(self):
         print()
         print("Current table:")
 
         for i in range(3):
-            print(self.table[3 * i], self.table[3 * i + 1], self.table[3 * i + 2])
+            print(TicTacToeServer.cell_content(self.table[3 * i], 3 * i),\
+                TicTacToeServer.cell_content(self.table[3 * i + 1], 3 * i + 1),\
+                TicTacToeServer.cell_content(self.table[3 * i + 2], 3 * i + 2))
 
         print()
-    
+
+    def is_finished(self):
+        # 0 - not finished, 1 - server (x) win, 2 - client(o) win 
+        
+
+
+        return 0
+
+    def start(self):
+        while self.is_finished() == 0:
+            print("Choose a cell (from 1 to 9): ", end=" ")
+            cell = int(input())
+
+            assert 1 <= cell <= 9
+
+            if self.table[cell - 1] != "o":
+                self.table[cell - 1] = "x"
+
+            self.show_table()
+
+            self.client_socket.send_int(cell)
+            client_cell = self.client_socket.recv_int()
+
+            self.table[client_cell - 1] = "o"
+
+            self.show_table()
+
+
 
 if __name__ == "__main__":
     server = TicTacToeServer()
     server.wait_for_tic_tac_toe_client()
 
-    server.show_table()
+    server.start()
 
     server.close()
 
