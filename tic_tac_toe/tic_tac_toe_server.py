d5e4fb90f962e313bf96358caf7baf81537742b3
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-05-21 17:37:59 +0300

    Create tic tac toe files

---
+++ tic_tac_toe/tic_tac_toe_server.py
@@ -0,0 +1,10 @@
+
+# because i want to call python server_main.py directly
+import sys
+sys.path.append("..")
+
+import socket
+
+from socket_helper.client_socket import *
+from socket_helper.server_socket import *
+
